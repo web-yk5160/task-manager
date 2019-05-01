d0a3fc71db82de211991df9686e0eb9891cb9362
web-yk5160 <rubystudy0123@gmail.com>
2019-05-02 00:24:04 +0900

    Adding and displaying task list

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
+    tasks: [
+      { id: 1, name: 'Todo1', description: 'This is a todo', completed: false },
+      { id: 2, name: 'Todo 2', description: 'This is another todo', completed: false },
+      { id: 3, name: 'Three', description: 'This is a complete todo', completed: true },
+      { id: 4, name: 'Four', description: 'This is another complete todo', completed: true }
+    ],
     message: 'Hello World!'
   }
 })
