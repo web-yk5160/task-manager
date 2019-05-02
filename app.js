fca0067b8545116512638eaf191f1949d14adef0
web-yk5160 <rubystudy0123@gmail.com>
2019-05-02 12:45:19 +0900

    Starting the task component

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var app = new Vue({
   el: '#app',
+  components: {
+    'task': {props: ['task'],
+    template: `
+              <div class="ui segment task"
+                          v-bind:class="task.completed ? 'done' : 'todo' ">
+                {{ task.id }}  {{ task.name }}: {{ task.description }} completed? {{ task.completed }}
+              </div>
+              `}
+  },
   data: {
     tasks: [
       { id: 1, name: 'Todo1', description: 'This is a todo', completed: false },
