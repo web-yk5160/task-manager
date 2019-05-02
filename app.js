72f17166a62526e6cb1c3f4d7152996c28165c56
web-yk5160 <rubystudy0123@gmail.com>
2019-05-02 14:16:11 +0900

    Finishing the Delete function

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ var app = new Vue({
               props: ['task'],
               template: `
               <div class="ui segment task"
-                          v-bind:class="task.completed ? 'done' : 'todo' ">
-                          <div class="ui grid">
-                            <div class="left floated twelve wide column">
-                              <div class="ui checkbox">
-                                <input type="checkbox" name="task" v-on:click="app.toggleDone($event, task.id)" :checked="task.completed">
-                                <label>{{ task.name }} <span class="description">{{ task.description }}</span></label>
-                              </div>
-                            </div>
-                            <div class="right floated three wide column">
-                              <i class="icon trash red" alt="Delete" v-on:click="app.deleteTask($event, task.id)"></i>
-                            </div>
-                          </div>
-                {{ task.id }}  {{ task.name }}: {{ task.description }} completed? {{ task.completed }}
+                    v-bind:class="task.completed ? 'done' : 'todo' ">
+                    <div class="ui grid">
+                      <div class="left floated twelve wide column">
+                        <div class="ui checkbox">
+                          <input type="checkbox" name="task" v-on:click="app.toggleDone($event, task.id)" :checked="task.completed">
+                          <label>{{ task.name }} <span class="description">{{ task.description }}</span></label>
+                        </div>
+                      </div>
+                      <div class="right floated three wide column">
+                        <i class="icon pencil blue" alt="Edit" v-on:click="app.editTask($event, task.id)"></i>
+                        <i class="icon trash red" alt="Delete" v-on:click="app.deleteTask($event, task.id)"></i>
+                      </div>
+                    </div>
               </div>
               `
             }
@@ -42,14 +42,30 @@ var app = new Vue({
   },
   methods: {
     toggleDone: function(event, id) {
+      event.stopImmediatePropagation();
+
       let task = this.tasks.find(item => item.id == id);
 
       if(task) {
-        task.completed = !task.completed;
-        console.log('task toggled');
+    task.completed = !task.completed;
+      console.log('task toggled');
+      }
+    },
+    editTask: function(event, id) {
+      let task = this.tasks.find(item => item.id == id);
+      if(task) {
+        console.log(task);
       }
     },
     deleteTask: function(event, id){
+      event.stopImmediatePropagation();
+
+      let taskIndex = this.tasks.findIndex(item => item.id == id);
+
+      if (taskIndex > -1) {
+        this.$delete(this.tasks, taskIndex);
+      }
+
       console.log('task deleted');
     }
   }
